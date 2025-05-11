b8deb98367386157a2b503df64c4200f158070a7
aaevans03 <aaevans03@gmail.com>
2025-05-10 20:12:14 -0600

    Basic implementation of boolean class finished

--- src/ChickenBouillon.java
+++ src/ChickenBouillon.java
@@ -1,3 +1,26 @@
 public class ChickenBouillon {
 
+    private boolean value;
+
+    public ChickenBouillon(boolean value) {
+        this.value = value;
+    }
+
+    enum BouillonValues {
+        TRUE,
+        FALSE
+    }
+
+    public void setValue(boolean value) {
+        this.value = value;
+    }
+
+    public boolean getValue () {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return (value ? "true" : "false" );
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        ChickenBouillon value = new ChickenBouillon(true);
+        System.out.println(value);
     }
 }
